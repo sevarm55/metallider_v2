9eea70ceede33d06ec1f5ff092319d52abfdcf30
Sevak <sevakrej@gmail.com>
2026-03-16 13:07:21 +0300

    change nextconfig fix img

--- next.config.ts
+++ next.config.ts
@@ -3,12 +3,7 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   images: {
     formats: ["image/avif", "image/webp"],
-    remotePatterns: [
-      {
-        protocol: "https",
-        hostname: "**",
-      },
-    ],
+    unoptimized: true,
   },
   async headers() {
     return [
